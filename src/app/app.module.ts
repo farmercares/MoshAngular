22ef7ac056dd6397485ed0cb99a05f60c92df9d1
farmercares <rfarmer@tacares.com>
2021-10-01 11:02:08 -0400

    Creating Components and Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
+import { CoursesService } from './courses.service';
+import { CoursesComponent } from './courses.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { CourseComponent } from './course/course.component';
+import { AuthorsService } from './authors.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    AuthorsComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CoursesService,
+    AuthorsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/courses.component.ts
@@ -0,0 +1,19 @@
+import { CoursesService } from './courses.service';
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'courses',
+    template: `
+    <h2>{{ title }}</h2>
+    <img src="{{ imageUrl }}" />
+    `
+})
+export class CoursesComponent {
+    title = "List of Courses";
+    courses;
+    imageUrl = "http://lorempixel.com/400/200";
+
+    constructor(service: CoursesService) {
+        this.courses = service.getCourses();
+    }
+}
