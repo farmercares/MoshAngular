dae502709d127e52200a0a8aba93261589779c29
farmercares <rfarmer@tacares.com>
2021-10-13 09:19:33 -0400

    FormArray Code
    Would need to be refactored for Angular 12. Very broken.

--- src/app/new-course-form-component/new-course-form-component.component.ts
+++ src/app/new-course-form-component/new-course-form-component.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-new-course-form-component',
   templateUrl: './new-course-form-component.component.html',
   styleUrls: ['./new-course-form-component.component.css']
 })
-export class NewCourseFormComponentComponent implements OnInit {
+export class NewCourseFormComponentComponent {
 
-  constructor() { }
+  topics: FormArray;
 
-  ngOnInit(): void {
+  form = new FormGroup({
+    topics: new FormArray([])
+  });
+  
+
+  addTopic(topic: HTMLInputElement){
+    this.topics.push(new FormControl(topic.value));
+    topic.value = '';
   }
 
+  removeTopic(topic: FormControl){
+    let index = this.topics.controls.indexOf(topic);
+    this.topics.removeAt(index);
+  }
+
+  getTopics() {
+    return this.form.get('topics') as FormArray;
+  }
 }
